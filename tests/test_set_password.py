c9ec47bacefa2c34c3952899f1245700abcc5dda
idav0 <20193tn142@utez.edu.mx>
2024-07-17 18:38:31 -0600

    Modules For Password Change (Temp & Permanent) added with password encrypted, Insert Users implements encryption too

--- tests/test_set_password.py
+++ tests/test_set_password.py
@@ -1,6 +1,6 @@
-from set_password import app
 import unittest
 import json
+import bcrypt
 
 
 mock_body = {
@@ -16,3 +16,17 @@ class TestApp(unittest.TestCase):
     def test_lambda_handler(self):
         result = app.lambda_handler(mock_body, None)
         print(result)
+
+    def test_bcrypt(self):
+
+        passw = 'ContraPrueba123.'
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(passw.encode('utf-8'), salt)
+        print(hashed)
+        hashed_decode = hashed.decode('utf-8')
+        print(hashed_decode)
+
+        confirm_old_password = bcrypt.checkpw(passw.encode('utf-8'), hashed_decode.encode('utf-8'))
+        print(confirm_old_password)
+
+
