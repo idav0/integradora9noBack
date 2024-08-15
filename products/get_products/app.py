37679466e7d88d357ffb0ec0679fa6255fe3de4d
idav0 <20193tn142@utez.edu.mx>
2024-08-14 19:41:36 -0600

    CORS fixed third try

--- products/get_products/app.py
+++ products/get_products/app.py
@@ -6,7 +6,7 @@ from shared.database_manager import DatabaseConfig
 
 cors_headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+    'Access-Control-Allow-Headers': '*',
     'Access-Control-Allow-Methods': 'OPTIONS,GET',
 }
 
